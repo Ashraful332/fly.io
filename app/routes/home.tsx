8db7c2dee66af37e9d2c4b0c5ea802dbe772357d
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-15 22:13:15 +0600

    init the project

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,6 @@ import "../app.css";
 
 export default function Home() {
   return <div>
-   <h1 className="text-amber-500">Hello World</h1>
+   <h1 className="text-amber-500 ">Hello World</h1>
   </div>
 }

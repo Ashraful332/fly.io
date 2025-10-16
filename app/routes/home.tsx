cfffbde8e9196aed9d11bf6014c2c41680bc8c86
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-16 16:25:37 +0600

    add all components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,30 @@
+import Navbar from "~/components/navigation/Navbar";
 import "../app.css";
+import Hero from "~/components/main/Hero";
+import Section1 from "~/components/main/Section1";
+import Footer from "~/components/navigation/Footer";
+import Section2 from "~/components/main/Section2";
+import Section3 from "~/components/main/Section3";
+import Section4 from "~/components/main/Section4";
+import Section5 from "~/components/main/Section5";
+import Section6 from "~/components/main/Section6";
+import Section7 from "~/components/main/Section7";
+
+
 
 export default function Home() {
   return <div>
-    <nav></nav>
-    <header></header>
+    <nav><Navbar/></nav>
+    <header><Hero/></header>
     <main>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
+      <section><Section1/></section>
+      <section><Section2/></section>
+      <section><Section3/></section>
+      <section><Section4/></section>
+      <section><Section5/></section>
+      <section><Section6/></section>
+      <section><Section7/></section>
     </main>
-    <footer></footer>
+    <footer><Footer/></footer>
   </div>
 }

f733d062a1527c63decf5fcfcae130710061aa6b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-18 16:35:02 +0600

    the work is almost don

--- app/components/main/Section5.tsx
+++ app/components/main/Section5.tsx
@@ -2,26 +2,26 @@
 
 const Section5 = () => {
     return (
-        <div>
+        <div className="bg-[var(--blue)] text-white py-40 ">
             <div className="max-w-[1200px] mx-auto pb-36 ">
                 <div className="flex justify-between">
                     <div>
-                        <h2>Use the Tech You Love</h2>
-                        <p>
+                        <h2 className="font-medium text-4xl mb-4 ">Use the Tech You Love</h2>
+                        <p className="text-[#cac8c8] mb-7 max-w-[512px] ">
                             Build with your favorite framework. No Dockerfile? No problem: our CLI generates containers for most popular frameworks, including Rails, Phoenix, Django, Node, Laravel, and .NET.
                         </p>
-                        <button>Learn More</button>
+                        <button className="bg-[#5495eb] text-white py-3 px-7 rounded-3xl ">Learn More</button>
                     </div>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
                     </div>
                 </div>
             </div>
--- app/components/main/Section6.tsx
+++ app/components/main/Section6.tsx
@@ -1,41 +1,42 @@
 import work from "~/assets/photo/working.png"
+import tick from "~/assets/icon/check.svg"
 
 
 const Section6 = () => {
     return (
-        <div className="max-w-[1200px] mx-auto pb-28  ">
+        <div className="max-w-[1200px] mx-auto pt-28 pb-28  ">
             <div className="flex justify-between">
                 <div className="max-w-[536px] ">
-                    <button>NEW!</button>
-                    <h2>Fly.io Managed Postgres</h2>
-                    <p>A fully-managed database service that handles all aspects of running production PostgreSQL where we take care of:</p>
-                    <div>
-                        <div>
-                            <img src="" alt="" />
+                    <button className="bg-[#ddb57149] py-1 px-3 text-xs rounded-2xl mb-4 ">NEW!</button>
+                    <h2 className="text-4xl font-medium mb-3 ">Fly.io Managed Postgres</h2>
+                    <p className="text-[#1E133C] mb-6 ">A fully-managed database service that handles all aspects of running production PostgreSQL where we take care of:</p>
+                    <div className="flex flex-col gap-3">
+                        <div className="flex items-center gap-3">
+                            <img src={tick} alt="icon" />
                             <p>Automatic backups and recovery</p>
                         </div>
-                        <div>
-                            <img src="" alt="" />
+                        <div className="flex items-center gap-3">
+                            <img src={tick} alt="icon" />
                             <p>High availability with automatic failover</p>
                         </div>
-                        <div>
-                            <img src="" alt="" />
+                        <div className="flex items-center gap-3">
+                            <img src={tick} alt="icon" />
                             <p>Performance monitoring and metrics</p>
                         </div>
-                        <div>
-                            <img src="" alt="" />
+                        <div className="flex items-center gap-3">
+                            <img src={tick} alt="icon" />
                             <p>Resource scaling (CPU, RAM, storage)</p>
                         </div>
-                        <div>
-                            <img src="" alt="" />
+                        <div className="flex items-center gap-3">
+                            <img src={tick} alt="icon" />
                             <p>24/7 support and incident response</p>
                         </div>
-                        <div>
-                            <img src="" alt="" />
+                        <div className="flex items-center gap-3">
+                            <img src={tick} alt="icon" />
                             <p>Automatic encryption of data at rest and in transit</p>
                         </div>
                     </div>
-                    <button>Learn More</button>
+                    <button className="bg-[var(--blue)] text-white py-3 px-7 rounded-3xl mt-8 " >Learn More</button>
                 </div>
                 <div >
                     <img src={work} alt="image" className="w-[385px] " />

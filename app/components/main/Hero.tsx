a86c4d8f11f1223275a2accb8fa0a183b77a9dd9
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-23 15:56:30 +0600

    the hero page background is add

--- app/components/main/Hero.tsx
+++ app/components/main/Hero.tsx
@@ -2,7 +2,7 @@
 
 const Hero = () => {
     return (
-        <div className="pt-32 pb-32 ">
+        <div className="pt-32 pb-32 h-[93vh] hero-background ">
             <div className="flex flex-col items-center justify-center max-w-[700px] mx-auto text-center font-medium">
                 <h1 className="text-6xl ">A Public Cloud Built For Developers Who Ship</h1>
                 <p className=" mt-5 mb-9 ">Over 3 million apps have launched on Fly.io, leveraging global Anycast load-balancing, zero-config private networking, hardware isolation, instant WireGuard VPN connections, and push-button deployments scaling to thousands of instances.</p>

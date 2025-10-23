896fea396de3da4d2cbcbbe8e302718f15444883
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-23 16:08:15 +0600

    add some logo

--- app/components/main/Section1.tsx
+++ app/components/main/Section1.tsx
@@ -6,14 +6,14 @@ const Section1 = () => {
     return (
         <div className="background-grid pt-32 ">
             <div className="max-w-[1200px] mx-auto pb-32  ">
-                <div className="flex justify-between ">
+                <div className="flex justify-between  ">
                     <div className="max-w-[536px] ">
                         <h2 className="mb-4 text-[#281950] text-4xl font-medium ">Public Cloud Infrastructure. Modern Platform Endorphins.</h2>
-                        <p className="text-[#1E133C] ">
+                        <p className="text-[#1e133cc9] leading-8 text-xl ">
                             The most flexible and powerful compute platform on any public cloud. Fly Machines are hardware-virtualized containers, running on our own hardware, that launch instantly and run exactly as long as you want them to — for a single HTTP request, or for weeks of uptime.
                         </p>
                     </div>
-                    <div>
+                    <div >
                         <img src={World} alt="world map illustration" className="w-[380px] h-[350px] object-cover " />
                     </div>
                 </div>
--- app/components/main/Section5.tsx
+++ app/components/main/Section5.tsx
@@ -1,4 +1,8 @@
-
+import logo from "~/assets/icon/go.svg"
+import logo1 from "~/assets/icon/Django.wine.svg"
+import logo2 from "~/assets/icon/laracel.svg"
+import logo3 from "~/assets/icon/docker.svg"
+import logo4 from "~/assets/icon/rust.svg"
 
 const Section5 = () => {
     return (
@@ -13,15 +17,33 @@ const Section5 = () => {
                         <button className="bg-[#5495eb] text-white py-3 px-7 rounded-3xl ">Learn More</button>
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
-                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] "></div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo} alt="logo" className=" h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo1} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo2} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo4} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo3} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3 ">
+                            <img src={logo} alt="logo" className="h-16 " />
+                        </div>
+                        <div className="w-40 h-24 bg-blue-400 rounded-2xl border border-[#ffffff5d] flex flex-col items-center p-3">
+                            <img src={logo} alt="logo" className="h-16 " />
+                        </div>
                     </div>
                 </div>
             </div>
